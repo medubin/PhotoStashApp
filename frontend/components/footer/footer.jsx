7b1c3a4e7fdbdf7e7cf60464ac9229c6b774bdac
medubin <medubin@gmail.com>
2016-03-19 15:17:47 -0700

    minor updates to header and footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -11,12 +11,21 @@ var Footer = React.createClass({
     window.location.href ='https://www.linkedin.com/in/medubin';
   },
 
+  forwardToAngelList: function() {
+    window.location.href ='https://angel.co/medubin';
+  },
+
+  forwardToEmail: function() {
+    window.location.href ='mailto:medubin@gmail.com';
+  },
 
   render: function() {
     return (
       <div className ='footer'>
-        <img onClick={this.forwardToGit} className='githublink' src='assets/GitHub-Mark-32px.png' width='18px'/>
-        <img onClick={this.forwardToLinkedin} className='linkedinlink' src='assets/linkedin.png' width='18px'/>
+        <img onClick={this.forwardToGit} className='githublink' src='assets/github-icon.png' width='40px'/>
+        <img onClick={this.forwardToLinkedin} className='linkedinlink' src='assets/linkedin-icon.png' width='40px'/>
+        <img onClick={this.forwardToAngelList} className='angellistlink' src='assets/angellist-icon.png' width='40px'/>
+        <img onClick={this.forwardToEmail} className='emaillink' src='assets/email-icon.png' width='40px'/>
       </div>
     );
   }
